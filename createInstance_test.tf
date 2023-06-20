5c3d2b75df97d7f8be0b1999da60a9d0c79d9018
okabb <oumaima.kaitbella@gmail.com>
2023-06-20 22:44:40 +0200

    add acc sec key

--- createInstance_test.tf
+++ createInstance_test.tf
@@ -4,8 +4,9 @@
 provider "aws" {
     version = "5.4.0"
     region     = "eu-west-1"
-    access_key = "**"
-    secret_key = "**"
+    access_key = "AKIA5J7JGEHJEDT5BT2B"
+    secret_key = "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"
+
 }
 
 resource "aws_instance" "TF_FirstInstnace" {
